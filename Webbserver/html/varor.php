d91029cbabb73a7d2513f6fcd1cbca96a7f9c9c2
GabrielRamde <54704377+GabrielRamde@users.noreply.github.com>
2020-11-06 10:43:34 +0100

    Add files via upload

--- Webbserver/html/varor.php
+++ Webbserver/html/varor.php
@@ -1,8 +1,9 @@
 <?php
-  $varor=array(
-    array("Äpple","Grönt surt","50", "bilder/apple.jpg"),
-    array("Apelsin","Orange söt", "38","bilder/orange.jpg"),
-    array("Päron","Gult saftigt", "100","bilder/pear.jpg"),
-    array("Banan","Gul böjd", "30","bilder/banana.jpg"),
-    array("Grapefrukt","Konstig", "300","bilder/banana.jpg"),
-  );
+	require "../includes/connect.php";
+	$sql = "SELECT * FROM products";
+		
+	$res = $dbh->prepare($sql);
+	$res->execute();
+	$result = $res->get_result();
+	$dbh->close();
+?>
