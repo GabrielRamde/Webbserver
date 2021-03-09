2efc307b4e81cd2f297dfc33d2e8911d78d966c7
GabrielRamde <54704377+GabrielRamde@users.noreply.github.com>
2021-03-09 14:33:30 +0100

    Add files via upload

--- Webbserver/template/menu.php
+++ Webbserver/template/menu.php
@@ -1,11 +1,39 @@
 <?php
-
-?>
-<nav><!--Navigationsmeny-->
-	<ul>	
-		<li><a href="index.php">Start</a></li>
-		<li><a href="products.php">Produkter</a></li>
-		<li><a href="sida3.php">Varusida 2</a></li>
-		<li><a href="login.php">Logga in</a></li>
-	</ul>
-</nav>
+	if(!isset($_SESSION['username'])) {
+		echo<<<NAV
+		<nav><!--Navigationsmeny-->
+			<ul>	
+				<li><a href="index.php">Start</a></li>
+				<li><a href="products.php">Produkter</a></li>
+				<li><a href="sida3.php">Varusida 2</a></li>
+				<li><a href="login.php">Logga in</a></li>
+			</ul>
+		</nav>
+NAV;
+	}
+	else{
+		if($_SESSION['status']==1){
+			echo<<<NAV
+			<nav>
+				<u1>
+					<li><a href="index.php">Start</a></li>
+					<li><a href="products.php">Produkter</a></li>
+					<li><a href="sida3.php">Varusida 2</a></li>
+					<li><a href="admin.php">Admin</a></li>
+				</ul>
+			</nav>
+NAV;
+	}
+	elseif($_SESSION['status']==2){
+			echo<<<NAV
+			<nav>
+				<u1>
+					<li><a href="index.php">Start</a></li>
+					<li><a href="products.php">Produkter</a></li>
+					<li><a href="createProduct.php">Skapa Produkt</a></li>
+					<li><a href="admin.php">Admin</a></li>
+				</ul>
+			</nav>
+NAV;
+		}
+	}
